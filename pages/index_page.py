d48d46787ec7aa1581a33115b301d95939af1d02
Fedor Belov <aror991@mail.ru>
2024-09-19 10:30:54 +0300

    complete programm

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,8 +1,46 @@
 from playwright.sync_api import Page
 import config
-
+import random
 
 class IndexPage:
-
+  
   def open_index_page(self, page: Page) -> None:
     page.goto(config.url.DOMAIN)
+    
+  def press_login_button(self, page: Page):
+    page.get_by_text("Login").click()
+    
+  def fill_login_data(self, page: Page):
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    
+  def press_add_to_cart(self, page: Page):
+    all_items = page.get_by_text("Add to cart").all()
+    if page.get_by_text("Remove").all() != 0:
+      for item in all_items:
+        if item.is_visible() and item.inner_text() == "Add to cart":
+          if random.choice([True, False]):
+            item.click()
+      
+  def press_open_cart(self, page:Page):
+    page.locator("[data-test=\"shopping-cart-link\"]").click()
+    
+  def press_checkout(self, page:Page):
+    page.locator("[data-test=\"checkout\"]").click()
+    
+  def fill_user_shipping_data(self, page:Page):
+    UFirstName = "John"
+    ULastName = "Abrams"
+    ZipCode = "100500"
+    page.get_by_placeholder("First Name").fill(UFirstName)
+    page.get_by_placeholder("Last Name").fill(ULastName)
+    page.get_by_placeholder("Zip/Postal Code").fill(ZipCode)
+    
+  def press_submit_cart(self, page:Page):
+    page.get_by_text("Continue").click()
+    
+  def complete_order(self, page:Page):
+    page.get_by_text("Finish").click()
+    
+  def check_order_completion(self, page:Page):
+    return page.locator("[data-test=\"complete-header\"]").inner_text()
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -6,4 +6,14 @@ class TestFooter:
 
     def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
         pages.index_page.open_index_page(page)
+        pages.index_page.fill_login_data(page)
+        pages.index_page.press_login_button(page)
+        pages.index_page.press_add_to_cart(page)
+        pages.index_page.press_open_cart(page)
+        pages.index_page.press_checkout(page)
+        pages.index_page.fill_user_shipping_data(page)
+        pages.index_page.press_submit_cart(page)
+        pages.index_page.complete_order(page)
+        result = pages.index_page.check_order_completion(page)
+        assert result == 'Thank you for your order!', "Order wasn't placed"
         time.sleep(10)
